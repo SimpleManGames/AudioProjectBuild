b939aa40ef3c06c20f7d5fab2169d564119e35ac
Riley Smith <riley.smith5345@gmail.com>
2025-05-15 23:35:05 -0700

    Build 4

---
+++ StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,104 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID AC_UNIT = 862742662U;
+        static const AkUniqueID AC_UNIT_LARGE = 478915366U;
+        static const AkUniqueID BOXPUSH = 4198514380U;
+        static const AkUniqueID CITY_BACKGROUND = 3516760007U;
+        static const AkUniqueID CLUBMUSIC = 1066508214U;
+        static const AkUniqueID DOOR_CLOSE = 128625444U;
+        static const AkUniqueID DOOR_OPEN = 535830432U;
+        static const AkUniqueID DOORSCANNER = 4056402093U;
+        static const AkUniqueID ENGINE_LARGETRUCK_IDLE = 4199429521U;
+        static const AkUniqueID ENGINE_SMALLTRUCK_IDLE = 1518239837U;
+        static const AkUniqueID ENGINE_SPORTS_IDLE = 3770289672U;
+        static const AkUniqueID ENGINE_SUPERCAR_IDLE = 4271490880U;
+        static const AkUniqueID HOVER_IDLE = 1245162470U;
+        static const AkUniqueID HOVER_REV = 1798144607U;
+        static const AkUniqueID NEONSIGN = 1445352338U;
+        static const AkUniqueID PLAY_FOOTSTEP = 1602358412U;
+        static const AkUniqueID PLAY_JUMP = 3689126666U;
+        static const AkUniqueID PLAY_NPC_FOOTSTEP = 3866133272U;
+        static const AkUniqueID STREETSWEEPERBRUSHES = 3744551435U;
+        static const AkUniqueID TESTINGEXPLOSION = 2883556008U;
+        static const AkUniqueID VENT = 3321711160U;
+    } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace FOOTSPTEPS
+        {
+            static const AkUniqueID GROUP = 739966622U;
+
+            namespace STATE
+            {
+                static const AkUniqueID ASPHALT = 4169408098U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID NONE = 748895195U;
+            } // namespace STATE
+        } // namespace FOOTSPTEPS
+
+    } // namespace STATES
+
+    namespace SWITCHES
+    {
+        namespace MATERIAL
+        {
+            static const AkUniqueID GROUP = 3865314626U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ASPHALT = 4169408098U;
+                static const AkUniqueID METAL = 2473969246U;
+                static const AkUniqueID TILE = 2637588553U;
+            } // namespace SWITCH
+        } // namespace MATERIAL
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID AUX = 983310469U;
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID WORLD = 2609808943U;
+    } // namespace BUSSES
+
+    namespace AUX_BUSSES
+    {
+        static const AkUniqueID REGION_ALCOVE = 4166818582U;
+        static const AkUniqueID REGION_BANK = 4142987362U;
+        static const AkUniqueID REGION_CITY = 1505882333U;
+        static const AkUniqueID REGION_CLUB = 1322167076U;
+        static const AkUniqueID REGION_DOCTORSOFFICE = 1889362552U;
+        static const AkUniqueID REGION_GARAGE = 3941196031U;
+        static const AkUniqueID REGION_SROOM = 318262586U;
+        static const AkUniqueID REGION_TUNNEL = 1869301910U;
+    } // namespace AUX_BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
